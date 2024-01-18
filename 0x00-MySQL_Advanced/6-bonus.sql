3f205964114bc968a00809d1f050f38332af7c61
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-18 21:37:27 +0300

    6.Add bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+-- A SQL scirpt that creates a stored procedure AddBonus that adds a new connection for a student
+DELIMITER //
+CREATE PROCEDURE AddBonus(
+	IN user_id INT,
+	IN project_name VARCHAR(225),
+	IN score FLOAT
+)
+BEGIN
+	IF NOT EXISTS (select * from projects where name = project_name)
+		THEN INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	set @id = (select id from projects where name = project_name);
+	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @id, score);
+END $$
+DELIMITER ;
