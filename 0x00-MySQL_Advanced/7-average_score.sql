ee73a68c92fd3ec6ff03386621f3d01b127733fb
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-18 21:53:35 +0300

    7.average score (corrected typo)

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,6 @@
 -- a SQL script that creates a stored procedure ComputerAverageScoreForUser that computes and store the average score for a student
 DELIMITER //
-CREATE PROCEDURE ComputerAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	SET @av = (SELECT AVG(score) AS av FROM corrections WHERE user_id=u_id
 	UPDATE users SET average_score= @av where id = u_id;
