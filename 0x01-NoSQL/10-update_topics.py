5c8382a9b91c7238fae265296e5a4d83af3d2ea6
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-23 21:51:46 +0300

    10. added a return statement

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,3 +10,4 @@ def update_topics(mongo_collection, name, topics):
     changes topos of a school document
     """
     updated = mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
+    updated.modified_count
